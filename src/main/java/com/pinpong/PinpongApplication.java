2b323d92238bb65d4608e58a3823314284f135a9
Ro1h7s7 <rodrigo_caceres@gap.com>
2017-04-27 15:51:07 -0300

    Initial workspace

---
+++ src/main/java/com/pinpong/PinpongApplication.java
@@ -0,0 +1,12 @@
+package com.pinpong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinpongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinpongApplication.class, args);
+	}
+}
